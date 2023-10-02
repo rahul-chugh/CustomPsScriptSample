d18e7eb3f1c4cd781fc27a6304a8c73f1b3813f5
Rahul Chugh <rahul.chugh@hotmail.com>
2023-10-02 14:21:44 -0700

    updating connection string

--- src/OnAfterInit.ps1
+++ src/OnAfterInit.ps1
@@ -14,3 +14,23 @@ users log in.
 #>
 
 Write-Host "Main script finished running at $(Get-Date)."
+
+Write-Host $env:USERPROFILE
+Set-Location $env:USERPROFILE
+[string] $workDirectory = "./AWS-workshop-assets"
+
+$sqlUsername = ((Get-SECSecretValue -SecretId "SQLServerRDSSecret").SecretString | ConvertFrom-Json).username
+$sqlPassword = ((Get-SECSecretValue -SecretId "SQLServerRDSSecret").SecretString | ConvertFrom-Json).password
+[string] $SQLDatabaseEndpoint = [System.Environment]::GetEnvironmentVariable("SQLDatabaseEndpoint")
+
+[string] $SQLDatabaseEndpointTrimmed = $SQLDatabaseEndpoint.Replace(':1433','')
+[string] $connectionString = "Server=$SQLDatabaseEndpointTrimmed;Database=GadgetsOnlineDB;User Id=$sqlUsername;Password=$sqlPassword;"
+
+[string] $webConfigFolder = "$workDirectory\dotnet-modernization-gadgetsonline/GadgetsOnline/GadgetsOnline"
+$webConfigPath = Join-Path $webConfigFolder "Web.config" 
+
+$xml = [xml](Get-Content -Path $webConfigPath)
+$xml.configuration.connectionStrings.add.connectionString = $connectionString
+$xml.Save($webConfigPath)
+
+Write-Host "Custom Script finished execution at $(Get-Date)."
