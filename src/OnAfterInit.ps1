9317b44b8dae85d08c5a7006cabd895ea557785f
Rahul Chugh <rahul.chugh@hotmail.com>
2023-10-03 10:43:55 -0700

    fixed user profile issue

--- src/OnAfterInit.ps1
+++ src/OnAfterInit.ps1
@@ -13,10 +13,25 @@ users log in.
 
 #>
 
+<#
+
+Feel free to modify this file by implementing your logic.
+
+This script is invoked after initialization scripts completed running 
+but before user has logged in.
+
+This script runs under Administrator account but without any UI, 
+meaning this script can't ask for user input. 
+
+It's OK for this script to run a few minutes, as it runs before
+users log in.
+
+#>
+
 Write-Host "Main script finished running at $(Get-Date)."
 
 Set-Location "C:\Users\Administrator"
-[string] $workDirectory = "./AWS-workshop-assets"
+[string] $workDirectory = "C:\Users\Administrator\AWS-workshop-assets"
 
 $sqlUsername = ((Get-SECSecretValue -SecretId "SQLServerRDSSecret").SecretString | ConvertFrom-Json).username
 $sqlPassword = ((Get-SECSecretValue -SecretId "SQLServerRDSSecret").SecretString | ConvertFrom-Json).password
@@ -25,7 +40,7 @@ $sqlPassword = ((Get-SECSecretValue -SecretId "SQLServerRDSSecret").SecretString
 [string] $SQLDatabaseEndpointTrimmed = $SQLDatabaseEndpoint.Replace(':1433','')
 [string] $connectionString = "Server=$SQLDatabaseEndpointTrimmed;Database=GadgetsOnlineDB;User Id=$sqlUsername;Password=$sqlPassword;"
 
-[string] $webConfigFolder = "$workDirectory\dotnet-modernization-gadgetsonline/GadgetsOnline/GadgetsOnline"
+[string] $webConfigFolder = "$workDirectory\dotnet-modernization-gadgetsonline\GadgetsOnline\GadgetsOnline"
 $webConfigPath = Join-Path $webConfigFolder "Web.config" 
 
 $xml = [xml](Get-Content -Path $webConfigPath)
