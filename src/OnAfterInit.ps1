438913fa2c949d17d5beff12d091eb22a27d609f
Rahul Chugh <rahul.chugh@hotmail.com>
2024-09-28 00:57:04 -0700

    added failsafe for db creation

--- src/OnAfterInit.ps1
+++ src/OnAfterInit.ps1
@@ -17,13 +17,37 @@ Write-Host "OnAfterInit script started running at $(Get-Date)."
 
 Set-Location "C:\Users\Administrator"
 [string] $workDirectory = "C:\Users\Administrator\AWS-workshop-assets"
+[string] $scriptPath = "$workDirectory\bobs-used-bookstore-classic\db-scripts\bobs-used-bookstore-classic-db.sql"
 
 $sqlUsername = ((Get-SECSecretValue -SecretId "SQLServerRDSSecret").SecretString | ConvertFrom-Json).username
 $sqlPassword = ((Get-SECSecretValue -SecretId "SQLServerRDSSecret").SecretString | ConvertFrom-Json).password
-[string] $SQLDatabaseEndpoint = [System.Environment]::GetEnvironmentVariable("SQLDatabaseEndpoint")
+
+$endpointAddress = Get-RDSDBInstance | Select-Object -ExpandProperty Endpoint | select Address
+[string] $SQLDatabaseEndpoint = $endpointAddress.Address
 
 [string] $SQLDatabaseEndpointTrimmed = $SQLDatabaseEndpoint.Replace(':1433','')
-[string] $connectionString = "Server=$SQLDatabaseEndpointTrimmed;Database=BookStoreClassic;User Id=$sqlUsername;Password=$sqlPassword;"
+
+# Set the database name
+$databaseName = "BookStoreClassic"
+
+# SQL query to check if the database exists
+$checkDbQuery = "IF EXISTS (SELECT name FROM sys.databases WHERE name = N'$databaseName') SELECT 1 ELSE SELECT 0"
+
+# Run the sqlcmd command to check if the database exists
+$checkDbResult = sqlcmd -U $sqlUsername -P $sqlPassword -S $SQLDatabaseEndpointTrimmed -Q $checkDbQuery -h -1 -W
+
+# Trim any whitespace from the result
+$checkDbResult = $checkDbResult.Trim()
+
+if ($checkDbResult -eq "0") {
+    Write-Host "Database '$databaseName' does not exist. Creating database..."
+    # Run the sqlcmd command to create the database
+    sqlcmd -U $sqlUsername -P $sqlPassword -S $SQLDatabaseEndpointTrimmed -i $scriptPath
+} else {
+    Write-Host "Database '$databaseName' already exists. Skipping creation."
+}
+
+[string] $connectionString = "Server=$SQLDatabaseEndpointTrimmed;Database=$databaseName;User Id=$sqlUsername;Password=$sqlPassword;"
 
 [string] $webConfigFolder = "$workDirectory\bobs-used-bookstore-classic\app\Bookstore.Web\"
 $webConfigPath = Join-Path $webConfigFolder "Web.config"
