29b16a860e0a0ce20d91c42a57a158b37c93987c
Rahul Chugh <rahul.chugh@hotmail.com>
2024-09-27 20:48:26 -0700

    scripts for bobs used bookstore classic connection string

--- src/OnAfterInit.ps1
+++ src/OnAfterInit.ps1
@@ -13,4 +13,29 @@ users log in.
 
 #>
 
-Write-Host "Main script finished running at $(Get-Date)."
+Write-Host "OnAfterInit script started running at $(Get-Date)."
+
+Set-Location "C:\Users\Administrator"
+[string] $workDirectory = "C:\Users\Administrator\AWS-workshop-assets"
+
+$sqlUsername = ((Get-SECSecretValue -SecretId "SQLServerRDSSecret").SecretString | ConvertFrom-Json).username
+$sqlPassword = ((Get-SECSecretValue -SecretId "SQLServerRDSSecret").SecretString | ConvertFrom-Json).password
+[string] $SQLDatabaseEndpoint = [System.Environment]::GetEnvironmentVariable("SQLDatabaseEndpoint")
+
+[string] $SQLDatabaseEndpointTrimmed = $SQLDatabaseEndpoint.Replace(':1433','')
+[string] $connectionString = "Server=$SQLDatabaseEndpointTrimmed;Database=BookStoreClassic;User Id=$sqlUsername;Password=$sqlPassword;"
+
+[string] $webConfigFolder = "$workDirectory\bobs-used-bookstore-classic\app\Bookstore.Web\"
+$webConfigPath = Join-Path $webConfigFolder "Web.config"
+
+$webConfigXml = [xml](Get-Content -Path $webConfigPath)
+
+$addElement = $webConfigXml.configuration.appSettings.add | Where-Object { $_.key -eq "ConnectionStrings/BookstoreDatabaseConnection" }
+$addElement.value = $connectionString
+
+$webConfigXml.Save($webConfigPath)
+
+$xml.configuration.connectionStrings.add.connectionString = $connectionString
+$xml.Save($webConfigPath)
+
+Write-Host "OnAfterInit script finished running at $(Get-Date)."
